f66cf6905e29ec3064dc7cebe59358107d117afe
Frie <fr1e@pm.me>
2019-07-08 14:26:20 +0200

    #21 change default of reverse arg of dodge2v to TRUE

--- R/position-dodge2v.R
+++ R/position-dodge2v.R
@@ -3,7 +3,7 @@
 #' @param padding Padding between elements at the same position. Elements are
 #'   shrunk by this proportion to allow space between them. Defaults to 0.1.
 position_dodge2v <- function(height = NULL, preserve = c("single", "total"),
-                             padding = 0.1, reverse = FALSE) {
+                             padding = 0.1, reverse = TRUE) {
   ggproto(NULL, PositionDodge2v,
     height = height,
     preserve = match.arg(preserve),
