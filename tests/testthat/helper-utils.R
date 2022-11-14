6b9c683e9a25f2d382617a2ed8bffbde1f339aaa
Lionel Henry <lionel.hry@gmail.com>
2022-11-14 14:45:17 +0100

    Update vdiffr

--- tests/testthat/helper-utils.R
+++ tests/testthat/helper-utils.R
@@ -1,27 +1,29 @@
 
 check_horizontal <- function(original, horizontal, fig_name,
                              skip_on_windows = FALSE) {
-  sort <- function(x) x[order(names(x))]
-  flipped <- function(fun) {
-    function(x, ...) fun(flip_aes(x), ...)
-  }
+  suppressWarnings({
+    sort <- function(x) x[order(names(x))]
+    flipped <- function(fun) {
+      function(x, ...) fun(flip_aes(x), ...)
+    }
 
-  set.seed(10)
-  h <- ggplot_build(original)
-  set.seed(10)
-  v <- ggplot_build(horizontal)
+    set.seed(10)
+    h <- ggplot_build(original)
+    set.seed(10)
+    v <- ggplot_build(horizontal)
 
-  # h_data <- lapply(h$data, flipped(sort))
-  # v_data <- lapply(v$data, sort)
-  # expect_identical(h_data, v_data)
+    # h_data <- lapply(h$data, flipped(sort))
+    # v_data <- lapply(v$data, sort)
+    # expect_identical(h_data, v_data)
 
-  if (skip_on_windows) {
-    skip_on_os("windows")
-  }
-  expect_doppelganger(fig_name, horizontal)
+    if (skip_on_windows) {
+      skip_on_os("windows")
+    }
+    expect_doppelganger(fig_name, horizontal)
+  })
 }
 
-expect_doppelganger <- function(title, fig, path = NULL, ...) {
+expect_doppelganger <- function(title, fig, ...) {
   testthat::skip_if_not_installed("vdiffr")
-  vdiffr::expect_doppelganger(title, fig, path = path, ...)
+  vdiffr::expect_doppelganger(title, fig, ...)
 }
